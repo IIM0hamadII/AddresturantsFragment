a860d9be51b0d8714673a05e1824919a2e325839
mohamad hosene <hamoudy1221h@gmail.com>
2023-10-19 14:48:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/addresturantsfragment/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.addresturantsfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gotoAddFragment();
+    }
+    private void gotoAddFragment(){
+        FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.FrameLayout,new AddRestaurantFragment());
+        ft.commit();
+    }
+}
