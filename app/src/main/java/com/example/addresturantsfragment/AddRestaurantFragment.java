94b2a6ec92dfbaf274903f4bb84f52f20bf93c8d
IIM0hamadII <hamoudy1221h@gmail.com>
2023-11-11 09:29:28 +0200

    commit

--- app/src/main/java/com/example/addresturantsfragment/AddRestaurantFragment.java
+++ app/src/main/java/com/example/addresturantsfragment/AddRestaurantFragment.java
@@ -1,10 +1,13 @@
 package com.example.addresturantsfragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +30,7 @@ public class AddRestaurantFragment extends Fragment {
     private FirebaseServices fbs;
     private EditText etName, etDescription, etAddress, etPhone;
     private Button btnAdd;
+    private Utils utils;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -90,7 +94,7 @@ public class AddRestaurantFragment extends Fragment {
         etAddress = getView().findViewById(R.id.etAddressAddRestaurantFragment);
         etPhone = getView().findViewById(R.id.etPhoneAddRestaurantFragment);
         btnAdd = getView().findViewById(R.id.btnAddAddRestaurantFragment);
-
+        utils = Utils.getInstance();
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -126,5 +130,21 @@ public class AddRestaurantFragment extends Fragment {
 
             }
         });
+
+    }
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == getActivity().RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            img.setImageURI(selectedImageUri);
+            utils.uploadImage(getActivity(), selectedImageUri);
+        }
     }
 }
