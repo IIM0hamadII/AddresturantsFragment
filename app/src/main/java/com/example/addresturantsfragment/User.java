81bb5fad042b9fe9d3c993a7fddeba5362b66b4a
IIM0hamadII <hamoudy1221h@gmail.com>
2024-03-05 11:06:36 +0200

    create user

---
+++ app/src/main/java/com/example/addresturantsfragment/User.java
@@ -0,0 +1,73 @@
+package com.example.addresturantsfragment;
+
+public class User {
+    private String name;
+    private String lastname;
+    private String hobbies;
+    private String phone;
+    private String livingarea;
+
+
+    public User(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", hobbies='" + hobbies + '\'' +
+                ", phone='" + phone + '\'' +
+                ", livingarea='" + livingarea + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLivingarea() {
+        return livingarea;
+    }
+
+    public void setLivingarea(String livingarea) {
+        this.livingarea = livingarea;
+    }
+
+    public User(String name, String lastname, String hobbies, String phone, String livingarea) {
+        this.name = name;
+        this.lastname = lastname;
+        this.hobbies = hobbies;
+        this.phone = phone;
+        this.livingarea = livingarea;
+    }
+}
